0c9822105ef5437b07f088dfee5c8cd96b3379e8
Andrea Coaker <andicoaker@gmail.com>
2017-09-11 19:09:53 -0400

    refactored comment_box_test user a forEach to render component before any of the it statements run

--- test/components/comment_box_text.js
+++ test/components/comment_box_text.js
@@ -3,20 +3,26 @@ import CommentBox from '../../src/components/comment_box';
 
 describe ('Text CommentBox', () => {
 
+  // declare variable outside of beforeEach then reassign varibale 'component' inside beforeEach
+  // variable starts out as undefined
+  let component;
+
+  beforeEach( () => {
+    component = renderComponent(CommentBox);
+
+  });
+
   it ('has the correct class comment-box', () =>{
-    const component = renderComponent(CommentBox);
     expect(component).to.have.class('comment-box');
   });
 
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     // chai doesn't require you to use a 2nd set of () around the 2nd part of the matcher
     // chai uses jQuery methods like .find below
     expect (component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 
