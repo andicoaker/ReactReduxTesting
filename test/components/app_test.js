2c299870bbd3154dd307df83a3b39940322deff4
Andrea Coaker <andicoaker@gmail.com>
2017-09-11 19:21:37 -0400

    added spec to App to make sure html generated is correct

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -4,19 +4,29 @@ import App from '../../src/components/app';
 // use 'describe' to group together similar tests
 describe('App', () => {
 
-  // use 'it' to test a single attribute of a target. returns in terminal like a report.
-  it ('shows the correct text', () => {
+  let component;
 
-    // create an instance of App
-    const component = renderComponent(App);
-
-    // use 'expect' to make an 'assertion' about a target
-    // using Mocha testing framework here
-    // expect is a function, in 1st part of assertion we pass expect the thing we want to make an assertion about - typically in react we are testing components
-    // 2nd part of assertion is a 'matcher' - this tells Mocha what we want to test and how it should behave. we pass this part of the assertion the value that we expect.
-
-    expect(component).to.contain('React simple starter');
-
+  beforeEach( () => {
+    component = renderComponent(App);
   });
+
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist
+  });
+
+  // use 'it' to test a single attribute of a target. returns in terminal like a report.
+  // it ('shows the correct text', () => {
+  //
+  //   // create an instance of App
+  //   const component = renderComponent(App);
+  //
+  //   // use 'expect' to make an 'assertion' about a target
+  //   // using Mocha testing framework here
+  //   // expect is a function, in 1st part of assertion we pass expect the thing we want to make an assertion about - typically in react we are testing components
+  //   // 2nd part of assertion is a 'matcher' - this tells Mocha what we want to test and how it should behave. we pass this part of the assertion the value that we expect.
+  //
+  //   expect(component).to.contain('React simple starter');
+  //
+  // });
 
 });
