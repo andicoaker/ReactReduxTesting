f55f4680531661dac2e4da9b642f73b17225add1
Andrea Coaker <andicoaker@gmail.com>
2017-09-12 17:03:30 -0400

    added specs for comment_box

--- test/components/comment_box_text.js
+++ test/components/comment_box_text.js
@@ -26,4 +26,18 @@ describe ('Text CommentBox', () => {
     expect(component.find('button')).to.exist;
   });
 
+  describe('entering some text', () => {
+
+    beforeEach( () =>{
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows that text is in the text area',() => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+
+    });
+  });
 });
