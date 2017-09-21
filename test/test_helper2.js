3ee32cbf6ea8764e9ec1a4a29ec0b13c2c7299b2
Andrea Coaker <andicoaker@gmail.com>
2017-09-21 15:52:15 -0400

    recreating test_helper from scratch, inside of new file test_helper2

---
+++ test/test_helper2.js
@@ -0,0 +1,15 @@
+
+
+// Set up testing environment to run like a browser in the command line
+
+
+
+// build 'renderComponent' helper that should render a given react ComponentClass
+
+
+
+// build helper for simulating events
+
+
+
+// set up chai-jquery
